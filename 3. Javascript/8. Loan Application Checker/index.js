844b72cd5b6c65ca0ea078096543bd74e7a8c3c4
Jozi Coder <jozicoder@gmail.com>
2025-09-17 15:26:08 +0200

    output the result (console) for each option

--- 3. Javascript/8. Loan Application Checker/index.js
+++ 3. Javascript/8. Loan Application Checker/index.js
@@ -27,3 +27,14 @@ function getLoanMessage(annualIncome, creditScore) {
     return "You don't qualify for any loans.";
   }
 }
+
+const duplexLoanMessage = getLoanMessage(85000, 850);
+const condoLoanMessage = getLoanMessage(65000, 690);
+const carLoanMessage = getLoanMessage(45000, 660);
+const noLoanMessage = getLoanMessage(25000, 550);
+
+//output
+console.log(duplexLoanMessage);
+console.log(condoLoanMessage);
+console.log(carLoanMessage);
+console.log(noLoanMessage);
