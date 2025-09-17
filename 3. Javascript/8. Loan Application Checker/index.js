5b39607363fc5dc166d81bb441f6ea6a8caf185a
Jozi Coder <jozicoder@gmail.com>
2025-09-17 15:17:30 +0200

    Create variables for different lending categories - 1. Duples, 2. Condo, 3. Car

---
+++ 3. Javascript/8. Loan Application Checker/index.js
@@ -0,0 +1,11 @@
+//Duplex
+const minIncomeForDuplex = 60000;
+const minCreditScoreForDuplex = 700;
+
+//Condo
+const minIncomeForCondo = 45000;
+const minCreditScoreForCondo = 680;
+
+//Car
+const minIncomeForCar = 30000;
+const minCreditForCar = 650;
