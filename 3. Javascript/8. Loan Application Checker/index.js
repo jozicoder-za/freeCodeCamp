35b6acb550df751213f218c26045f69b5294e438
Jozi Coder <jozicoder@gmail.com>
2025-09-17 15:25:04 +0200

    create loan vetting logic if if/else statement

--- 3. Javascript/8. Loan Application Checker/index.js
+++ 3. Javascript/8. Loan Application Checker/index.js
@@ -9,3 +9,21 @@ const minCreditScoreForCondo = 680;
 //Car
 const minIncomeForCar = 30000;
 const minCreditForCar = 650;
+
+function getLoanMessage(annualIncome, creditScore) {
+  if (
+    annualIncome > minIncomeForDuplex &&
+    creditScore > minCreditScoreForDuplex
+  ) {
+    return "You qualify for a duplex, condo and car loan.";
+  } else if (
+    annualIncome > minIncomeForCondo &&
+    creditScore > minCreditScoreForCondo
+  ) {
+    return "You qualify for a condo and car loan";
+  } else if (annualIncome > minIncomeForCar && creditScore > minCreditForCar) {
+    return "You qualify for a car loan.";
+  } else {
+    return "You don't qualify for any loans.";
+  }
+}
