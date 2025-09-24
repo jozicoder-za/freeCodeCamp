2b71ac5120396cfa3f46965fb7fc37d3b1b9aad6
Jozi Coder <jozicoder@gmail.com>
2025-09-24 16:16:01 +0200

    golf holes nicknames defined

---
+++ 3. Javascript/11. Golf Score Translator/index.js
@@ -0,0 +1,15 @@
+const names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home!",
+];
+
+function golfScore(par, strokes) {
+  let result;
+
+  return result;
+}
