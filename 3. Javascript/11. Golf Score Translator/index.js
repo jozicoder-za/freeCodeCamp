f33ce67984f28122a9ec4f5a45c578f0b73a2297
Jozi Coder <jozicoder@gmail.com>
2025-09-24 16:17:21 +0200

    create function logic to evaluate golf par

--- 3. Javascript/11. Golf Score Translator/index.js
+++ 3. Javascript/11. Golf Score Translator/index.js
@@ -10,6 +10,20 @@ const names = [
 
 function golfScore(par, strokes) {
   let result;
-
+  if (strokes === 1) {
+    result = names[0];
+  } else if (strokes <= par - 2) {
+    result = names[1];
+  } else if (strokes === par - 1) {
+    result = names[2];
+  } else if (strokes === par) {
+    result = names[3];
+  } else if (strokes === par + 1) {
+    result = names[4];
+  } else if (strokes === par + 2) {
+    result = names[5];
+  } else if (strokes >= par + 3) {
+    result = names[6];
+  }
   return result;
 }
