463fc7503abf8600cdd351ffe1d668392f143bb4
Jozi Coder <jozicoder@gmail.com>
2025-10-12 11:48:11 +0200

    add array items removal functionality from the end and beginning

--- 3. Javascript/4. Array/2. Lunch Picker Program/index.js
+++ 3. Javascript/4. Array/2. Lunch Picker Program/index.js
@@ -8,6 +8,7 @@ function addLunchToEnd(arr, str) {
 }
 
 console.log(addLunchToEnd(lunches, "Pizza"));
+console.log(addLunchToEnd(lunches, "Chicken Curry"));
 
 // Step 3
 function addLunchToStart(arr, str) {
@@ -16,3 +17,26 @@ function addLunchToStart(arr, str) {
 }
 
 console.log(addLunchToStart(lunches, "Pasta"));
+
+// Step 4
+function removeLastLunch(arr) {
+  if (arr.length < 1) {
+    return `No lunches to remove`;
+  }
+
+  let lastItem = arr.pop();
+  return `${lastItem} removed from the end of the lunch menu`;
+}
+
+console.log(removeLastLunch(lunches));
+
+// Step 5
+function removeFirstLunch(arr) {
+  if (arr.length < 1) {
+    return `No Lunches to remove`;
+  }
+  let firstItem = arr.shift();
+  return `${firstItem} removed from the start of the lunch menu.`;
+}
+
+console.log(removeFirstLunch(lunches));
